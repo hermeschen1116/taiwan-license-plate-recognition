f8372179e5df5c04989d1511ee56275189251d0a
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-17 16:34:01 +0800

    feat(src): add some scripts and helper functions

---
+++ src/scripts/train.py
@@ -0,0 +1,37 @@
+import os
+
+from dotenv import load_dotenv
+from huggingface_hub import hf_hub_download
+from roboflow import Roboflow
+from ultralytics import YOLO
+
+from license_plate_recognition.helper import get_num_of_workers, get_torch_device
+
+load_dotenv()
+
+roboflow_agent = Roboflow(api_key=os.environ.get("ROBOFLOW_API_KEY"))
+
+dataset = (
+	roboflow_agent.workspace("work-c9x8f")
+	.project("taiwan-license-plate-recognition")
+	.version(2)
+	.download("yolov8-obb", location="../datasets/roboflow")
+)
+
+model = YOLO(hf_hub_download("Ultralytics/YOLOv8", filename="yolov8n.pt"))
+
+config: dict = {"epochs": 100, "optimzer": "auto"}
+
+result = model.train(
+	data=f"{dataset.location}/data.yaml",
+	batch=-1,
+	imgsz=500,
+	save_period=1,
+	cache="disk",
+	device=get_torch_device(),
+	workers=get_num_of_workers(),
+	single_cls=True,
+	resume=True,
+	profile="onnx",
+	plots=True,
+)
