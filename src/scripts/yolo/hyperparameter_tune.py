c7ff4698f9bb70077f4a5721cf274183116544d2
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-25 13:29:19 +0800

    refactor: add openvino dep

---
+++ src/scripts/yolo/hyperparameter_tune.py
@@ -0,0 +1,42 @@
+import os
+
+import wandb
+from dotenv import load_dotenv
+from roboflow import Roboflow
+from ultralytics import YOLO
+
+from license_plate_recognition.helper import get_num_of_workers, get_torch_device
+
+load_dotenv()
+
+project_root: str = os.environ.get("PROJECT_ROOT", "")
+
+wandb.login(key=os.environ.get("WANDB_API_KEY"))
+
+roboflow_agent = Roboflow(api_key=os.environ.get("ROBOFLOW_API_KEY"))
+
+dataset = (
+	roboflow_agent.workspace("jackresearch0")
+	.project("taiwan-license-plate-recognition-research-tlprr")
+	.version(7)
+	.download("yolov8-obb", location=f"{project_root}/datasets/roboflow")
+)
+
+model = YOLO(f"{project_root}/models/yolov8n-obb.pt")
+
+model.tune(
+	project="taiwan-license-plate-recognition",
+	data=f"{dataset.location}/data.yaml",
+	epochs=10,
+	iterations=300,
+	imgsz=640,
+	optimizer="AdamW",
+	plots=True,
+	save=False,
+	val=False,
+	cache="disk",
+	device=get_torch_device(),
+	workers=get_num_of_workers(),
+)
+
+wandb.finish()
---
+++ src/scripts/yolo/train.py
@@ -0,0 +1,45 @@
+import os
+
+import wandb
+from dotenv import load_dotenv
+from roboflow import Roboflow
+from ultralytics import YOLO
+
+from license_plate_recognition.helper import get_num_of_workers, get_torch_device
+
+load_dotenv()
+
+project_root: str = os.environ.get("PROJECT_ROOT", "")
+
+wandb.login(key=os.environ.get("WANDB_API_KEY"))
+
+roboflow_agent = Roboflow(api_key=os.environ.get("ROBOFLOW_API_KEY"))
+
+dataset = (
+	roboflow_agent.workspace("jackresearch0")
+	.project("taiwan-license-plate-recognition-research-tlprr")
+	.version(7)
+	.download("yolov8-obb", location=f"{project_root}/datasets/roboflow")
+)
+
+model = YOLO(f"{project_root}/models/yolov8n-obb.pt")
+
+config: dict = {"epochs": 1, "optimizer": "auto"}
+
+result = model.train(
+	project="taiwan-license-plate-recognition",
+	data=f"{dataset.location}/data.yaml",
+	batch=-1,
+	imgsz=640,
+	save_period=1,
+	cache="disk",
+	device=get_torch_device(),
+	workers=get_num_of_workers(),
+	single_cls=True,
+	plots=True,
+	**config,
+)
+
+model.val()
+
+model.export(format="openvino", imgsz=640, half=True, int8=True, batch=1, dynamic=True, device="cpu")
