3af8476495ca675a1ba7f776ab5cd417760261c8
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-25 16:48:11 +0800

    refactor: rename package

--- src/scripts/ocr/fine_tune.py
+++ src/scripts/ocr/fine_tune.py
@@ -1,11 +1,11 @@
+from license_plate_recognition.helper import get_num_of_workers
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 from datasets import load_dataset
-from license_plate_recognition.helper import get_num_of_workers
 
 num_workers: int = get_num_of_workers()
 
-dataset = load_dataset("gagan3012/IAM", keep_in_memory=True, num_proc=num_workers)
+dataset = load_dataset("gagan3012/IAM", keep_in_memory=True, num_proc=num_workers)["train"].remove_columns(["label"])
 
 tokenizer = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
 
