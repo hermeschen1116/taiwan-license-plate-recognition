0e97582354643460103a56abb326d646e2e4b6d9
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-11-22 13:15:56 +0800

    feat(src/scripts/yolo/evaluate.py): implement evaluate script for yolo in OpenVINO format

---
+++ src/scripts/yolo/evaluate.py
@@ -0,0 +1,42 @@
+import os
+
+import wandb
+from dotenv import load_dotenv
+from roboflow import Roboflow
+from ultralytics import YOLO
+from wandb.integration.ultralytics import add_wandb_callback
+
+load_dotenv()
+
+project_root: str = os.environ.get("PROJECT_ROOT", "")
+
+wandb.login(key=os.environ.get("WANDB_API_KEY"))
+
+run = wandb.init(project="taiwan-license-plate-recognition", job_type="evaluate")
+
+roboflow_agent = Roboflow(api_key=os.environ.get("ROBOFLOW_API_KEY"))
+
+dataset = (
+	roboflow_agent.workspace("work-c9x8f")
+	.project("license-plate-detection-mdsot")
+	.version(6)
+	.download("yolov8-obb", location=f"{project_root}/datasets/roboflow")
+)
+
+model_path: str = run.use_model("license-plate-detection:latest").download()
+
+model = YOLO(model_path, task="obb")
+
+add_wandb_callback(model, enable_model_checkpointing=True, visualize_skeleton=True)
+
+result = model.val(
+	project="taiwan-license-plate-recognition",
+	data=f"{dataset.location}/data.yaml",
+	batch=-1,
+	imgsz=640,
+	device="cpu",
+	split="test",
+	plots=True,
+)
+
+wandb.finish()
