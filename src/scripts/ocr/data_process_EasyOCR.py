23c22f0f1a8da088ccb31ce5d763d3e1e7f005ac
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-11-28 11:47:52 +0800

    WIP

--- src/scripts/ocr/data_process_EasyOCR.py
+++ src/scripts/ocr/data_process_EasyOCR.py
@@ -6,6 +6,7 @@ import polars
 from dotenv import load_dotenv
 
 from datasets import DatasetDict
+from datasets.features.image import Image
 
 load_dotenv()
 
@@ -15,11 +16,13 @@ data_directory: str = f"{project_root}/datasets/ocr"
 
 with tempfile.TemporaryDirectory() as temp_dir:
 	data: List[Dict[str, str]] = [
-		{"label": file.split(".")[0], "file": os.path.join(data_directory, file)} for file in os.listdir(data_directory)
+		{"label": file.split(".")[0], "image": os.path.join(data_directory, file)}
+		for file in os.listdir(data_directory)
 	]
 	polars.DataFrame(data).write_csv(f"{temp_dir}/data.csv")
 	dataset = DatasetDict.from_csv({"train": f"{temp_dir}/data.csv"})
 	dataset["train"], dataset["validation"] = dataset["train"].train_test_split(test_size=0.2).values()
 	dataset["train"], dataset["test"] = dataset["train"].train_test_split(test_size=0.125).values()
+	dataset = dataset.cast_column("image", Image(decode=False))
 
-	print(dataset["train"])
+	print(dataset["train"][0])
