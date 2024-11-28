0a02b065621215c71e2010d6beac0c17efff677b
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-11-28 16:51:57 +0800

    style: format code

--- src/scripts/ocr/train_easyOCR.py
+++ src/scripts/ocr/train_easyOCR.py
@@ -1,7 +1,9 @@
 import os
 import tempfile
+from pprint import pprint
 
 from dotenv import load_dotenv
+from tqdm.auto import tqdm
 
 from datasets import Image, load_dataset
 from taiwan_license_plate_recognition.helper import get_num_of_workers
@@ -12,16 +14,15 @@ project_root: str = os.environ.get("PROJECT_ROOT", "")
 num_workers: int = get_num_of_workers()
 
 dataset = load_dataset("hermeschen1116/taiwan-license-plate-ocr", num_proc=num_workers)
-dataset = dataset.remove_columns(["label_other"]).unique("label")
+dataset = dataset.remove_columns(["label_other"])
 dataset = dataset.cast_column("image", Image(decode=True))
 
-dataset_directory = tempfile.TemporaryDirectory()
-for split in ["train", "validation", "test"]:
-	os.makedirs(f"{dataset_directory}/{split}/images")
-	for sample in dataset[split]:
-		sample["image"].save(f"{dataset_directory}/{split}/images/{sample['label']}")
-	dataset[split].to_csv(f"{dataset_directory}/{split}/label.csv", num_proc=num_workers)
+with tempfile.TemporaryDirectory() as dataset_directory:
+	for split in ["train", "validation", "test"]:
+		os.makedirs(f"{dataset_directory}/{split}/images")
+		for sample in tqdm(dataset[split], desc=f"{split}: ", colour="green"):
+			sample["image"].save(f"{dataset_directory}/{split}/images/{sample['label']}.png")
+		dataset[split].to_csv(f"{dataset_directory}/{split}/label.csv", num_proc=num_workers)
 
-	os.listdir(f"{dataset_directory}/{split}/")
-
-dataset_directory.cleanup()
+		pprint(os.listdir(f"{dataset_directory}/{split}/"))
+		pprint(os.listdir(f"{dataset_directory}/{split}/images")[0:10])
