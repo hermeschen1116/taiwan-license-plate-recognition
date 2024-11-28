c28d49cb0f347187949799621974f53eff3dbb05
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-11-28 11:44:21 +0800

    WIP

---
+++ src/scripts/ocr/data_process_EasyOCR.py
@@ -0,0 +1,25 @@
+import os
+import tempfile
+from typing import Dict, List
+
+import polars
+from dotenv import load_dotenv
+
+from datasets import DatasetDict
+
+load_dotenv()
+
+project_root: str = os.environ.get("PROJECT_ROOT", "")
+
+data_directory: str = f"{project_root}/datasets/ocr"
+
+with tempfile.TemporaryDirectory() as temp_dir:
+	data: List[Dict[str, str]] = [
+		{"label": file.split(".")[0], "file": os.path.join(data_directory, file)} for file in os.listdir(data_directory)
+	]
+	polars.DataFrame(data).write_csv(f"{temp_dir}/data.csv")
+	dataset = DatasetDict.from_csv({"train": f"{temp_dir}/data.csv"})
+	dataset["train"], dataset["validation"] = dataset["train"].train_test_split(test_size=0.2).values()
+	dataset["train"], dataset["test"] = dataset["train"].train_test_split(test_size=0.125).values()
+
+	print(dataset["train"])
