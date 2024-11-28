fbf103fbcbec268c7459289ec2cd52c288bcee20
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-11-28 14:18:44 +0800

    perf(src/scripts/ocr/data_process_EasyOCR.p): rewrite data process script

--- src/scripts/ocr/data_process_EasyOCR.py
+++ src/scripts/ocr/data_process_EasyOCR.py
@@ -1,4 +1,5 @@
 import os
+import random
 import shutil
 import tempfile
 from typing import Dict, List
@@ -6,7 +7,7 @@ from typing import Dict, List
 import polars
 from dotenv import load_dotenv
 
-from datasets import DatasetDict
+from datasets import load_dataset
 from datasets.features.image import Image
 from taiwan_license_plate_recognition.helper import get_num_of_workers
 
@@ -18,18 +19,31 @@ num_workers: int = get_num_of_workers()
 data_source: str = f"{project_root}/datasets/ocr"
 
 with tempfile.TemporaryDirectory() as temp_dir:
-	data_directory: str = f"{temp_dir}/data_source"
-	shutil.copytree(data_source, data_directory, dirs_exist_ok=True)
+	files: List[str] = os.listdir(data_source)
+	num_files: int = len(files)
 
-	data: List[Dict[str, str]] = [
-		{"label": file.split(".")[0], "image": os.path.join(data_directory, file)}
-		for file in os.listdir(data_directory)
-	]
-	polars.DataFrame(data).write_csv(f"{temp_dir}/data.csv")
+	files = random.sample(files, num_files)
 
-	dataset = DatasetDict.from_csv({"train": f"{temp_dir}/data.csv"})
-	dataset["train"], dataset["validation"] = dataset["train"].train_test_split(test_size=0.2).values()
-	dataset["train"], dataset["test"] = dataset["train"].train_test_split(test_size=0.125).values()
+	num_train_split: int = int(num_files * 0.7)
+	num_validation_split: int = int(num_files * 0.2)
+	splits: Dict[str, List[str]] = {
+		"train": files[:num_train_split],
+		"validation": files[num_train_split : (num_train_split + num_validation_split)],
+		"test": files[(num_train_split + num_validation_split) :],
+	}
+
+	dataset_path: str = f"{temp_dir}/datasets"
+	os.makedirs(dataset_path)
+	for split_name in splits.keys():
+		os.makedirs(f"{dataset_path}/{split_name}")
+		for file in splits[split_name]:
+			shutil.copyfile(os.path.join(data_source, file), f"{dataset_path}/{split_name}/{file}")
+		metadata: List[Dict[str, str]] = [
+			{"file_name": file, "label": file.split(".")[0]} for file in splits[split_name]
+		]
+		polars.DataFrame(metadata).write_csv(f"{dataset_path}/{split_name}/metadata.csv")
+
+	dataset = load_dataset("imagefolder", data_dir=dataset_path, save_infos=True, num_proc=num_workers)
 	dataset = dataset.cast_column("image", Image(decode=False))
 
 	def contain_chinese(s: str) -> bool:
