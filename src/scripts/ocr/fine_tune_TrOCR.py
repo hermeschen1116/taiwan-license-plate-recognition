2bcf40b60dd4335559d3c8914f96a39cb62dc36b
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-05 09:23:00 +0800

    fix(src/scripts/ocr/fine_tune_TrOCR.py): fix bugs

--- src/scripts/ocr/fine_tune_TrOCR.py
+++ src/scripts/ocr/fine_tune_TrOCR.py
@@ -5,13 +5,7 @@ import evaluate
 import torch
 from PIL.Image import Resampling
 from dotenv import load_dotenv
-from transformers import (
-	GenerationConfig,
-	Seq2SeqTrainer,
-	Seq2SeqTrainingArguments,
-	TrOCRProcessor,
-	VisionEncoderDecoderModel,
-)
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments, TrOCRProcessor, VisionEncoderDecoderModel
 
 import wandb
 from datasets import Image, load_dataset
@@ -22,6 +16,7 @@ load_dotenv()
 project_root: str = os.environ.get("PROJECT_ROOT", "")
 num_workers: int = get_num_of_workers()
 device: str = get_torch_device()
+max_length: int = 64
 
 run = wandb.init(job_type="fine_tune", project="taiwan-license-plate-recognition", group="TrOCR", mode="offline")
 
@@ -29,6 +24,7 @@ processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-printed", clean
 
 dataset = load_dataset("hermeschen1116/taiwan-license-plate-ocr", keep_in_memory=True, num_proc=num_workers)
 dataset = dataset.remove_columns(["label_other"])
+print(dataset)
 
 dataset = dataset.cast_column("image", Image(decode=True))
 
@@ -50,7 +46,7 @@ dataset = dataset.rename_column("image", "pixel_values")
 
 
 def encode_label(label):
-	encoded_label = processor.tokenizer(label, padding="max_length", max_length=64).input_ids
+	encoded_label = processor.tokenizer(label, padding="max_length", max_length=max_length).input_ids
 	encoded_label[encoded_label == processor.tokenizer.pad_token_id] = -100
 	return encoded_label
 
@@ -66,56 +62,48 @@ dataset.set_format("torch", columns=["pixel_values", "labels"], output_all_colum
 model = VisionEncoderDecoderModel.from_pretrained(
 	"DunnBC22/trocr-base-printed_license_plates_ocr", torch_dtype=torch.bfloat16, low_cpu_mem_usage=True
 )
+# set special tokens used for creating the decoder_input_ids from the labels
+model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
+model.config.pad_token_id = processor.tokenizer.pad_token_id
+# make sure vocab size is set correctly
 model.config.vocab_size = model.config.decoder.vocab_size
 
-
-generation_config = (
-	GenerationConfig(
-		decoder_start_token_id=processor.tokenizer.cls_token_id,
-		pad_token_id=processor.tokenizer.pad_token_id,
-		eos_token_id=processor.tokenizer.sep_token_id,
-		max_length=64,
-		early_stopping=True,
-		no_repeat_ngram_size=3,
-		length_penalty=2.0,
-		num_beams=4,
-	),
-)
+# set beam search parameters
+model.config.eos_token_id = processor.tokenizer.sep_token_id
+model.config.max_length = max_length
+model.config.early_stopping = True
+model.config.no_repeat_ngram_size = 3
+model.config.length_penalty = 2.0
+model.config.num_beams = 4
 
 
 trainer_arguments = Seq2SeqTrainingArguments(
-	output_dir="./checkpoints",
+	output_dir=f"./checkpoints/{run.name}",
 	overwrite_output_dir=True,
 	eval_strategy="steps",
 	per_device_train_batch_size=4,
 	per_device_eval_batch_size=4,
 	eval_accumulation_steps=50,
-	torch_empty_cache_steps=100,
+	run_name=run.name,
 	eval_delay=500,
-	# learning_rate=2e-4,
-	# weight_decay=0.001,
-	# max_grad_norm=0.3,
-	num_train_epochs=1,
-	lr_scheduler_type="linear",
-	# warmup_ratio=0.003,
+	num_train_epochs=10,
+	lr_scheduler_type="reduce_lr_on_plateau",
 	logging_steps=25,
 	save_steps=25,
 	save_total_limit=5,
 	load_best_model_at_end=True,
 	metric_for_best_model="cer",
+	greater_is_better=False,
 	bf16=True,
 	fp16=False,
-	dataloader_num_workers=num_workers,
-	optim="paged_adamw_32bit",
+	optim="paged_lion_32bit",
 	group_by_length=True,
 	report_to=["wandb"],
 	dataloader_pin_memory=True,
-	dataloader_persistent_workers=True,
 	auto_find_batch_size=True,
 	eval_on_start=True,
 	sortish_sampler=True,
 	predict_with_generate=True,
-	generation_config=generation_config,
 )
 
 cer_metric = evaluate.load("cer", keep_in_memory=True)
@@ -142,9 +130,12 @@ trainer = Seq2SeqTrainer(
 
 trainer.train()
 
-evaluate_result = trainer.evaluate(dataset["test"])
-
 model = torch.compile(model, dynamic=True, backend="openvino", mode="reduce-overhead")
+
+evaluate_result = trainer.evaluate(dataset["test"], metric_key_prefix="test")
+run.log(evaluate_result)
+
+model.push_to_hub("taiwan-license-plate-recognition")
 
 wandb.finish()
 
