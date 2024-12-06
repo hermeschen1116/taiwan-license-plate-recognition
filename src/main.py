3f9ac628453dc02c223eecfa3b52ad5776aed111
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-06 16:34:52 +0800

    feat(src/main.py): implement detect script

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import logging
+import os
+from typing import List
+
+from dotenv import load_dotenv
+from optimum.intel import OVModelForVision2Seq, OVWeightQuantizationConfig
+from transformers import TrOCRProcessor
+from ultralytics import YOLO
+
+from taiwan_license_plate_recognition.Utils import extract_license_number, extract_license_plate
+
+load_dotenv()
+
+program_name: str = "LICENSE NUMBER RECOGNIZER"
+yolo_model_path: str = os.environ.get("YOLO_MODEL_PATH", "")
+logging.info(f"{program_name}: YOLO_MODEL_PATH: {yolo_model_path}")
+ocr_processor_path: str = os.environ.get("OCR_PROCESSOR_PATH", "microsoft/trocr-base-printed")
+logging.info(f"{program_name}: OCR_PROCESSOR_PATH: {ocr_processor_path}")
+ocr_model_path: str = os.environ.get("OCR_MODEL_PATH", "DunnBC22/trocr-base-printed_license_plates_ocr")
+logging.info(f"{program_name}: OCR_MODEL_PATH: {ocr_model_path}")
+stream_path: str = os.environ.get("CAMERA_ADDRESS", "")
+logging.info(f"{program_name}: CAMERA_ADDRESS: {stream_path}")
+
+logging.info(f"{program_name}: Initialize YOLO model.")
+yolo_model = YOLO(yolo_model_path, task="obb")
+
+logging.info(f"{program_name}: Initialize TROCR processor.")
+ocr_processor = TrOCRProcessor.from_pretrained(ocr_processor_path, clean_up_tokenization_spaces=True)
+
+quantization_config = OVWeightQuantizationConfig()
+ov_config = {"PERFORMANCE_HINT": "LATENCY"}
+
+logging.info(f"{program_name}: Initialize TROCR model.")
+ocr_model = OVModelForVision2Seq.from_pretrained(
+	ocr_model_path, export=True, ov_config=ov_config, quantization_config=quantization_config, device="cpu"
+)
+
+logging.info(f"{program_name}: Start detecting")
+for result in yolo_model.predict(stream_path, stream=True, device="cpu"):
+	cropped_images = extract_license_plate(result)
+	license_numbers: List[str] = extract_license_number(cropped_images, ocr_model, ocr_processor)
+	logging.info(f"{program_name}: License number: {', '.join(license_numbers)}")
--- src/taiwan_license_plate_recognition/Utils.py
+++ src/taiwan_license_plate_recognition/Utils.py
@@ -1,20 +1,32 @@
-from typing import Generator, List
+from typing import List
 
 import cv2
 from PIL import Image
 
-from taiwan_license_plate_recognition.PostProcess import crop_image
+from taiwan_license_plate_recognition.PostProcess import crop_image, remove_non_alphanum
 from taiwan_license_plate_recognition.Preprocess import affine_transform
 
 
-def extract_license_plate(results: Generator) -> List[Image.Image]:
+def extract_license_plate(result) -> List[Image.Image]:
 	license_plates: List[Image.Image] = []
 
-	for result in results:
-		for obb in result.obb.xyxyxyxy:
-			cropped_image = crop_image(result.orig_img, obb)
-			cropped_image, _ = affine_transform(cropped_image, 384)
-			cropped_image = Image.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
-			license_plates.append(cropped_image)
+	for obb in result.obb.xyxyxyxy:
+		cropped_image = crop_image(result.orig_img, obb)
+		cropped_image, _ = affine_transform(cropped_image, 384)
+		cropped_image = Image.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+		license_plates.append(cropped_image)
 
 	return license_plates
+
+
+def extract_license_number(images: List[Image.Image], model=None, processor=None, max_length: int = 64) -> List[str]:
+	if len(images) == 0:
+		return []
+
+	encode_image = processor(images, return_tensors="pt").pixel_values
+
+	generated_ids = model.generate(encode_image, max_length=max_length)
+
+	results = processor.batch_decode(generated_ids, skip_special_tokens=True)
+
+	return [remove_non_alphanum(result) for result in results]
