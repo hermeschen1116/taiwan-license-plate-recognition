7eb89b0ec50252706605af9ba254fdd6005d7173
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-12 21:42:51 +0800

    perf(src/taiwan_license_plate_recognition/PostProcess.py): simply filter_license_number

--- src/taiwan_license_plate_recognition/PostProcess.py
+++ src/taiwan_license_plate_recognition/PostProcess.py
@@ -1,6 +1,5 @@
 import re
 from string import punctuation
-from typing import List
 
 import cv2
 import numpy
@@ -17,19 +16,14 @@ def remove_non_alphanum(s: str) -> str:
 	return s.translate(str.maketrans("", "", punctuation)).replace(" ", "")
 
 
-def filter_license_number(candidates: List[str]) -> str:
-	candidates_without_hyphen: List[str] = [remove_non_alphanum(candidate) for candidate in candidates]
+def filter_license_number(candidate: str) -> str:
+	if (
+		re.match(r"^[A-Z\d]{2}-[A-Z\d]{4}$", candidate) is None
+		and re.match(r"^[A-Z\d]{4}-[A-Z\d]{2}$", candidate) is None
+		and re.match(r"^[A-Z\d]{3}-[A-Z\d]{3}$", candidate) is None
+		and re.match(r"^[A-Z\d]{3}-[A-Z\d]{4}$", candidate) is None
+		and re.match(r"^[A-Z\d]{6,7}$", candidate) is None
+	):
+		return ""
 
-	filter_candidates: List[str] = []
-	for i, (c, cwh) in enumerate(zip(candidates, candidates_without_hyphen)):
-		if (
-			re.match(r"^[A-Z\d]{2}-[A-Z\d]{4}$", c) is None
-			and re.match(r"^[A-Z\d]{4}-[A-Z\d]{2}$", c) is None
-			and re.match(r"^[A-Z\d]{3}-[A-Z\d]{3}$", c) is None
-			and re.match(r"^[A-Z\d]{3}-[A-Z\d]{4}$", c) is None
-		):
-			continue
-
-		filter_candidates.append(cwh.replace("-", ""))
-
-	return filter_candidates[0] if len(set(filter_candidates)) >= 1 else ""
+	return candidate.replace("-", "")
