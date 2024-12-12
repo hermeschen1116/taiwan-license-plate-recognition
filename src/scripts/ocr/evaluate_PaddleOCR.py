87b04d62cc80c2023137d41b60a6e1557b85dc3b
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-12 21:46:02 +0800

    style: format code

--- src/scripts/ocr/evaluate_PaddleOCR.py
+++ src/scripts/ocr/evaluate_PaddleOCR.py
@@ -3,11 +3,11 @@ import os
 import cv2
 import evaluate
 import numpy
-import wandb
 from dotenv import load_dotenv
 from paddleocr import PaddleOCR
 
 import datasets
+import wandb
 from datasets import load_dataset
 from taiwan_license_plate_recognition.Helper import accuracy_metric, get_num_of_workers
 from taiwan_license_plate_recognition.Utils import extract_license_number_paddleocr
