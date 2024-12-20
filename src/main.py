fd0e750501c27b9c2ac047e362c6a7996d002325
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-20 09:47:05 +0800

    refactor(src/main.py): introduce some new env

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import os
 import time
-from typing import List
+from typing import Generator, List
 
 import cv2
 import paddle
 import requests
+from cv2.typing import MatLike
 from dotenv import load_dotenv
 from paddleocr import PaddleOCR
 from ultralytics import YOLO
@@ -17,20 +18,20 @@ load_dotenv()
 paddle.disable_signal_handler()
 
 project_root: str = os.environ.get("PROJECT_ROOT", "")
-
-num_workers: int = get_num_of_workers()
 program_name: str = "LICENSE NUMBER RECOGNIZER"
 
-image_size: int = int(os.environ.get("IMAGE_SIZE", 640))
+inference_device: str = os.environ.get("INFERENCE_DEVICE", "cpu")
+num_workers: int = int(os.environ.get("NUM_WORKERS", get_num_of_workers()))
+frame_size: int = int(os.environ.get("FRAME_SIZE", 640))
 stream_path: str = os.environ.get("STREAM_SOURCE", "")
 detection_model_path: str = os.environ.get("DETECTION_MODEL_PATH", "")
 api_endpoint: str = os.environ.get("API_ENDPOINT", "")
 
-yolo_model = YOLO(detection_model_path, task="obb")
+yolo_model: YOLO = YOLO(detection_model_path, task="obb")
 
-reader = PaddleOCR(
+reader: PaddleOCR = PaddleOCR(
 	lang="en",
-	device="cpu",
+	device=inference_device,
 	use_angle_cls=True,
 	max_text_length=8,
 	total_process_num=num_workers,
@@ -39,14 +40,14 @@ reader = PaddleOCR(
 	binarize=True,
 )
 
-stream = cv2.VideoCapture(stream_path)
-stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc("M", "J", "P", "G"))
-stream.set(cv2.CAP_PROP_FRAME_WIDTH, image_size)
-stream.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size)
+stream: cv2.VideoCapture = cv2.VideoCapture(stream_path)
+stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
+stream.set(cv2.CAP_PROP_FRAME_WIDTH, frame_size)
+stream.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_size)
 stream.set(cv2.CAP_PROP_FPS, 1)
 
 while stream.isOpened():
-	key = cv2.waitKey(90)
+	key: int = cv2.waitKey(90)
 	if key == ord("q") or key == 27:
 		stream.release()
 		break
@@ -56,8 +57,8 @@ while stream.isOpened():
 		continue
 	cv2.imwrite(f"{project_root}/log/{time.time()}.png", frame)
 
-	detections = yolo_model.predict(frame, imgsz=image_size, half=True, device="cpu")
-	cropped_images = extract_license_plate(detections, image_size)
+	detections: Generator = yolo_model.predict(frame, imgsz=frame_size, half=True, device=inference_device)
+	cropped_images: List[MatLike] = extract_license_plate(detections, frame_size)
 	license_numbers: List[str] = list(filter(None, extract_license_number(cropped_images, reader)))
 	if len(license_numbers) == 0:
 		continue
