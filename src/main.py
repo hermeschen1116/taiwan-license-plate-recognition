3fac1367963630c8450a8a8dfe661147ed9fb0f0
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-20 14:45:55 +0800

    fix: load model and stream without async

--- src/main.py
+++ src/main.py
@@ -17,31 +17,27 @@ from taiwan_license_plate_recognition import (
 	recognize_license_number,
 	send_results,
 )
-from taiwan_license_plate_recognition.Utils import get_num_of_workers
 
 
 async def main() -> None:
 	load_dotenv()
 
 	inference_device: str = os.environ.get("INFERENCE_DEVICE", "cpu")
-	num_workers: int = int(os.environ.get("NUM_WORKERS", get_num_of_workers()))
 	frame_size: int = int(os.environ.get("FRAME_SIZE", 640))
 	api_endpoint: str = os.environ.get("API_ENDPOINT", "")
 
-	detection_model: YOLO = await load_detection_model()
+	detection_model: YOLO = load_detection_model()
 
-	recognition_model: PaddleOCR = await load_recognition_model(
+	recognition_model: PaddleOCR = load_recognition_model(
 		lang="en",
 		binarize=True,
 		use_angle_cls=True,
 		max_text_length=8,
 		use_space_char=False,
 		device=inference_device,
-		use_mp=True,
-		total_process_num=num_workers,
 	)
 
-	stream: cv2.VideoCapture = await initialize_stream(frame_size)
+	stream: cv2.VideoCapture = initialize_stream(frame_size)
 
 	frame_queue: asyncio.Queue = asyncio.Queue()
 	image_queue: asyncio.Queue = asyncio.Queue()
