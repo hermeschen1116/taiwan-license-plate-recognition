a740008b1b683f5694d5cdf853adf2c1cdbd9001
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2025-01-09 11:14:47 +0800

    refactor(pyproject.toml): specify dep group for paddlepaddle

--- src/scripts/recognition/data_process.py
+++ src/scripts/recognition/data_process.py
@@ -4,10 +4,10 @@ import tempfile
 from typing import Dict, List
 
 import polars
-from datasets import DatasetDict
-from datasets.features.image import Image
 from dotenv import load_dotenv
 
+from datasets import DatasetDict
+from datasets.features.image import Image
 from taiwan_license_plate_recognition.Utils import get_num_of_workers
 
 load_dotenv()
